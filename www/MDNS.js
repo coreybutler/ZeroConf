8fb28c0d68f3716175da35ebffb5d16486994f3b
coreybutler <=corey@coreybutler.cmom>
2014-05-07 19:39:54 -0500

    Tweaks

--- www/MDNS.js
+++ www/MDNS.js
@@ -1,53 +1,55 @@
 var exec = require('cordova/exec');
 
-var MDNS = function(_type){
+var MDNS = function(type){
+
+  type = type || "_http._tcp.";
 
   Object.defineProperties(this,{
-    handlers:{
-      enumerable: false,
-      writable: true,
-      configurable: true,
-      value: {}
-    },
-    oncehandlers:{
-      enumerable: false,
-      writable: true,
-      configurable: true,
-      value: {}
-    },
-    on: {
-      enumerable: true,
-      writable: false,
-      configurable: false,
-      value: function(eventName,fn){
-        this.handlers[eventName] = this.handlers[eventName] || [];
-        this.handlers[eventName].push(fn);
-      }
-    },
-    once: {
-      enumerable: true,
-      writable: false,
-      configurable: false,
-      value: function(eventName,fn){
-        this.oncehandlers[eventName] = this.oncehandlers[eventName] || [];
-        this.oncehandlers[eventName].push(fn);
-      }
-    },
-    handleEvent: {
-      enumerable: false,
-      writable: false,
-      configurable: false,
-      value: function(data){
-        (this.handlers[data.action]||[]).forEach(function(handler){
-          alert(data);
-          handler.apply(me,[data]);
-        });
-        (this.oncehandlers[data.action]||[]).forEach(function(handler){
-          handler.apply(me,[data]);
-        });
-        this.oncehandlers.hasOwnProperty(data.action) && delete this.oncehandlers[data.action];
-      }
-    },
+//    handlers:{
+//      enumerable: false,
+//      writable: true,
+//      configurable: true,
+//      value: {}
+//    },
+//    oncehandlers:{
+//      enumerable: false,
+//      writable: true,
+//      configurable: true,
+//      value: {}
+//    },
+//    on: {
+//      enumerable: true,
+//      writable: false,
+//      configurable: false,
+//      value: function(eventName,fn){
+//        this.handlers[eventName] = this.handlers[eventName] || [];
+//        this.handlers[eventName].push(fn);
+//      }
+//    },
+//    once: {
+//      enumerable: true,
+//      writable: false,
+//      configurable: false,
+//      value: function(eventName,fn){
+//        this.oncehandlers[eventName] = this.oncehandlers[eventName] || [];
+//        this.oncehandlers[eventName].push(fn);
+//      }
+//    },
+//    handleEvent: {
+//      enumerable: false,
+//      writable: false,
+//      configurable: false,
+//      value: function(data){
+//        (this.handlers[data.action]||[]).forEach(function(handler){
+//          alert(data);
+//          handler.apply(me,[data]);
+//        });
+//        (this.oncehandlers[data.action]||[]).forEach(function(handler){
+//          handler.apply(me,[data]);
+//        });
+//        this.oncehandlers.hasOwnProperty(data.action) && delete this.oncehandlers[data.action];
+//      }
+//    },
     listening: {
       enumerable: false,
       writable: true,
@@ -58,12 +60,11 @@ var MDNS = function(_type){
       enumerable: true,
       writable: false,
       configurable: false,
-      value: function(type){
+      value: function(callback){
         if (this.listening) {
           return;
         }
 
-        type = type || _type || "_http._tcp.";
         var me = this;
 
         this.listening = true;
@@ -73,12 +74,7 @@ var MDNS = function(_type){
             action: result,
           };
           alert('JAVA CALLED: '+data.action);
-          if (type !== null){
-            data.action === type && me.handleEvent(data);
-            return;
-          } else {
-            me.handleEvent(data);
-          }
+          callback && callback(data);
         }, function(e){
           throw e;
         }, "MDNS", "monitor", [type]);
